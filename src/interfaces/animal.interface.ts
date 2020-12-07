c37a8538db881e471987743f7672158997caa29e
Andres Granja <andresgranja001@gmail.com>
2020-12-07 13:10:23 -0500

    code uploaded

---
+++ src/interfaces/animal.interface.ts
@@ -0,0 +1,8 @@
+
+export interface animal {
+    nombre: string;
+    imagen: string;
+    audio: string;
+    duracion: number;
+    reproduciendo: boolean;
+}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ANIMALES } from '../../data/data.animales';
+import { animal } from '../../interfaces/animal.interface';
+import { Refresher, reorderArray } from 'ionic-angular';
+
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +12,62 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  animales: animal[] = [];
+  audio = new Audio();
+  audioTiempo: any;
+  ordenando: boolean = false;
+  
   constructor(public navCtrl: NavController) {
+    this.animales = ANIMALES.slice(0);
+  }
 
+  reproducir(animal: animal) {
+
+    this.pausar_audio(animal);
+
+    if (animal.reproduciendo == true) {
+      animal.reproduciendo = false;
+      return;
+    }
+
+    console.log(animal);
+
+    this.audio.src = animal.audio;
+
+    this.audio.load();
+    this.audio.play();
+
+    animal.reproduciendo = true;
+
+    this.audioTiempo = setTimeout(() => animal.reproduciendo = false, animal.duracion * 1000);
+  }
+
+  private pausar_audio(animalSel: animal) {
+    clearTimeout(this.audioTiempo);
+    this.audio.pause();
+    this.audio.currentTime = 0;
+
+    for (let animal of this.animales) {
+
+      if (animal.nombre != animalSel.nombre) {
+        animal.reproduciendo = false;
+      }
+    }
+  }
+
+  borrar_animal(idx: number) {
+    this.animales.splice(idx, 1);
+  }
+
+  recargar_animales(refresher: Refresher) {
+    setTimeout(() => {
+      this.animales = ANIMALES.slice(0);
+      refresher.complete();
+    }, 1000);
+  }
+
+  reordenar_animales(indice: any) {
+    this.animales = reorderArray(this.animales, indice);
   }
 
 }
